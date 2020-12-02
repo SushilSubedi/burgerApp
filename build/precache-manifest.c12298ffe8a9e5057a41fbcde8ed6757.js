8d0e8aa6ed2ab54375ba9d79cd9e61d21efcc90d
Sushil Subedi <sushilsubedi151@gmail.com>
2020-12-02 11:53:12 -0500

    burger app

---
+++ build/precache-manifest.c12298ffe8a9e5057a41fbcde8ed6757.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875fd78a18ae23c5d28bf3b22bb0194a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e13b9cd5a734294b05f",
+    "url": "/static/css/3.4daee03a.chunk.css"
+  },
+  {
+    "revision": "a7b3bd6885cd295f34cd",
+    "url": "/static/css/4.79c75930.chunk.css"
+  },
+  {
+    "revision": "5b6752208b686ad83c5b",
+    "url": "/static/css/5.182185f7.chunk.css"
+  },
+  {
+    "revision": "dfab91084556a3165c53",
+    "url": "/static/css/main.879b03ef.chunk.css"
+  },
+  {
+    "revision": "f694c0ce5c98987724f5",
+    "url": "/static/js/2.0e25d8b5.chunk.js"
+  },
+  {
+    "revision": "8e13b9cd5a734294b05f",
+    "url": "/static/js/3.7ed7e5e4.chunk.js"
+  },
+  {
+    "revision": "a7b3bd6885cd295f34cd",
+    "url": "/static/js/4.c2428d40.chunk.js"
+  },
+  {
+    "revision": "5b6752208b686ad83c5b",
+    "url": "/static/js/5.16e93195.chunk.js"
+  },
+  {
+    "revision": "dfab91084556a3165c53",
+    "url": "/static/js/main.b90bfa5d.chunk.js"
+  },
+  {
+    "revision": "33221eb325f22a20bfbf",
+    "url": "/static/js/runtime~main.2861da28.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
